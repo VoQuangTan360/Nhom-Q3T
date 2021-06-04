c7e9a0e630b0914e3777ea7af8230c214b32cb5d
VoQuangTan360 <80733265+VoQuangTan360@users.noreply.github.com>
2021-06-04 12:41:05 +0700

    Create taikhoan.java

---
+++ code/taikhoan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Admin
+ */
+public class taikhoan implements Serializable{
+    private String taiKhoan,matKhau;
+
+    public String getTaiKhoan() {
+        return taiKhoan;
+    }
+
+    public void setTaiKhoan(String taiKhoan) {
+        this.taiKhoan = taiKhoan;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+}
